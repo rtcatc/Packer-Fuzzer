cee1b0c2996bcdcd635adb0e15fdf036e77410d2
Poc Sir <Cool-Guy@C-est.Cool>
2021-10-09 11:04:12 +0800

    v1.3.5 bug fix

--- lib/LoadExtensions.py
+++ lib/LoadExtensions.py
@@ -15,7 +15,7 @@ class loadExtensions():
         sys.path.append(path)
         for root, dirs, files in os.walk(r"ext"):
             for file in files:
-                if os.path.join(root,file).split(os.sep)[-1].split(".")[-1] == "py":
+                if os.path.join(root,file).split(os.sep)[-1].split(".")[-1] == "py" and file != '__init__.py':
                     module_name = os.path.join(root,file).split(os.sep)[-1].split(".")[0]
                     module = __import__(module_name)
                     module.ext(self.projectTag, self.options).start()
